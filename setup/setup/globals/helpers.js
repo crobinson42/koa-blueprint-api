9086eec0eaf720f2c1d6dd84b62186ac19d6bee8
Cory Robinson <coryrobinson42@gmail.com>
2018-10-13 10:47:06 -0700

    chore(): eslint

--- setup/setup/globals/helpers.js
+++ setup/setup/globals/helpers.js
@@ -1,59 +1,63 @@
-const fs = require("fs");
-const path = require("path");
-const debug = require("debug")("kba:setup:globals");
+const fs = require('fs')
+const path = require('path')
+const debug = require('debug')('kba:setup:globals')
 
 module.exports.loadGlobals = function loadGlobals(name, fileList = []) {
-  if (!global[name]) global[name] = {};
+  if (!global[name]) global[name] = {}
 
-  if (!fileList.length) return;
+  if (!fileList.length) return
 
-  debug(`Loading globals for ${name}:`);
+  debug(`Loading globals for ${name}:`)
 
   fileList.forEach(fileInfo => {
-    let value;
+    let value
 
     try {
       // eslint-disable-next-line
-      value = require(fileInfo.path);
+      value = require(fileInfo.path)
     } catch (e) {
       debug(
         `Failed to load file "${fileInfo.file}" for "${name}" @ ${
           fileInfo.path
-        }`,
-        e
-      );
+        } ${e}`
+      )
       throw new Error(
         `Failed to load file "${fileInfo.file}" for "${name}" @ ${
           fileInfo.path
-        }`,
-        e
-      );
-      process.exit(1);
+        } ${e}`
+      )
+      process.exit(1)
     }
 
     Object.defineProperty(global[name], fileInfo.name, {
       enumerable: true,
       value,
-      writable: false
-    });
+      writable: false,
+    })
 
-    debug(` - ${fileInfo.name}`);
-  });
-};
+    debug(` - ${fileInfo.name}`)
+  })
+}
 
-module.exports.getDirFileList = function getDirFileList(dirPath) {
+module.exports.getDirFileList = function getDirFileList(
+  dirPath,
+  opts = { throw: false },
+) {
   try {
     return fs.readdirSync(dirPath).map(file => ({
       file,
       name: file.substr(0, file.search(/\.js$/)),
-      path: path.resolve(path.join(dirPath, file))
-    }));
+      path: path.resolve(path.join(dirPath, file)),
+    }))
   } catch (e) {
-    debug(`Failed to read directorty ${dirPath} in utils/setup.js`, e);
-    throw new Error(
-      `Failed to read directorty ${dirPath} in utils/setup.js`,
-      e
-    );
-    return process.exit(1);
+    if (opts.throw) {
+      debug(`Failed to read directory ${dirPath} in utils/setup.js`, e)
+      throw new Error(
+        `Failed to read directorty ${dirPath} in utils/setup.js ${e}`
+      )
+    }
+
+    debug(`Directory does not exist, ${dirPath}`)
+    return []
   }
-};
+}
